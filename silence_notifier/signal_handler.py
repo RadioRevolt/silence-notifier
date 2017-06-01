4f12977af5632a26b90ff986c0c73840450bfa69
Thorben Werner Sjøstrøm Dahl <thorben@sjostrom.no>
2017-06-01 18:28:01 +0200

    Protect against multiple SIGTERM arriving rapidly

--- silence_notifier/signal_handler.py
+++ silence_notifier/signal_handler.py
@@ -1,16 +1,25 @@
+import logging
 import signal
+import threading
 
 
 def create_registerer(plugin):
     """Bind plugin so it is used when the returned function is called by the
     system.
     """
+    lock = threading.Lock()
+
     def handle_sigterm(signum, frame):
         """Catch a sigterm and handle the processing to plugin"""
-        # Send our good bye message
-        plugin.handle_sigterm()
-        # Halt execution
-        raise KeyboardInterrupt()
+        # Protect so only one sigterm is handled, not two at once
+        if lock.acquire(blocking=False):
+            logging.debug("First SIGTERM received.")
+            # Send our good bye message
+            plugin.handle_sigterm()
+            # Halt execution
+            raise KeyboardInterrupt()
+        else:
+            logging.debug("SIGTERM received, but we are already handling one.")
     return handle_sigterm
 
 
